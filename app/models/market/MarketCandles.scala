6b1a85e6dc7f0a06498d7e378de516097cded66c
Flowmaster <ellyssin.gimhae@gmail.com>
2016-10-11 17:43:19 -0600

    Moved candle management to a model class.

---
+++ app/models/market/MarketCandles.scala
@@ -0,0 +1,125 @@
+package models.market
+
+import com.typesafe.scalalogging.LazyLogging
+import models.poloniex.{MarketCandle, MarketEvent, MarketUpdate}
+import services.actors.ExponentialMovingAverageActor.MarketCandleClose
+import services.actors.PoloniexCandleRetrieverActor.QueueMarket
+import utils.Misc
+
+import scala.collection.mutable.ListBuffer
+
+/**
+  * Created by bishop on 10/11/16.
+  */
+class MarketCandles(val periodMinutes: Int = 5) extends LazyLogging {
+  require(periodMinutes >= 5)
+
+  val marketCandles = scala.collection.mutable.Map[String, ListBuffer[MarketCandle]]()
+
+  /**
+    * Adds the market candles to collection.
+    * @param marketName
+    * @param last24hrCandles
+    */
+  def appendCandles(marketName: String, last24hrCandles: List[MarketCandle]) = {
+    marketCandles.get(marketName) match {
+      case Some(candles) =>
+        logger.info(s"Retrieved candle data for $marketName")
+        // This assumes that the candles received are sorted by time descending
+        val lastCandle = last24hrCandles.head
+        val currentCandle = candles.last
+        if (currentCandle.time.equals(lastCandle.time)) {
+          currentCandle.addCandle(lastCandle)
+          candles.appendAll(last24hrCandles.takeRight(last24hrCandles.length - 1))
+        } else {
+          candles.appendAll(last24hrCandles)
+        }
+      case None =>
+        logger.error(s"received $marketName ${last24hrCandles.length} but nowhere to add them??")
+    }
+
+  }
+
+  def getLatestCandle(marketName: String): Option[MarketCandle] = {
+    marketCandles.get(marketName) match {
+      case Some(list) => Some(list.head)
+      case None => None
+    }
+  }
+
+  def getMarketCandles(marketName: String): List[MarketCandle] = {
+    marketCandles.get(marketName) match {
+      case Some(list) => list.toList.reverse
+      case None => List[MarketCandle]()
+    }
+  }
+
+  def containsMarket(marketName: String): Boolean = {
+    marketCandles.contains(marketName)
+  }
+
+  def updateMarket(update: MarketUpdate)(closeFn: (MarketCandleClose) => Unit) = {
+    val name = update.name
+    // only care about BTC markets
+    if (name.startsWith("BTC")) {
+
+      // this is the current time rounded down to 5 minutes
+      val time = Misc.currentTimeRoundedDown(periodMinutes)
+
+      // get candle collection for this market
+      val candleBuffer = marketCandles.get(name) match {
+        case Some(candles) => candles
+        case None =>
+          val candles = scala.collection.mutable.ListBuffer.empty[MarketCandle]
+          marketCandles.put(name, candles)
+          candles
+      }
+
+      if (candleBuffer.length > 0) {
+        // examine candle at head
+        val currentCandle = candleBuffer.head
+
+        // are we still in the time period of the last candle that we created
+        if (currentCandle.time.isEqual(time)) {
+          // if so we need to update the info for the last candle
+          currentCandle.updateStatus(update.info)
+        } else {
+          // if we've skipped some candle periods due to no trade activity
+          // we need to fill in those missed periods
+          val skippedCandles = ((time.toEpochSecond - currentCandle.time.toEpochSecond) / (periodMinutes * 60L)).toInt - 1
+          for (i <- 1 to skippedCandles) {
+            // add appropriate 5 minute interval to compute next candle time
+
+            val candleTime = currentCandle.time.plusMinutes(periodMinutes * i)
+            val candle = MarketCandle(candleTime, periodMinutes, currentCandle.close)
+
+            // inform the moving average actor of new close period
+            closeFn(MarketCandleClose(name, ClosePrice(candleTime, currentCandle.close)))
+            //eventBus.publish(MarketEvent("/market/candle/close", MarketCandleClose(name, ClosePrice(candleTime, currentCandle.close))))
+            candleBuffer.insert(0, candle)
+          }
+
+          // start a new candle
+          val candle = MarketCandle(time, periodMinutes, update.info.last)
+          candle.updateStatus(update.info)
+          // most recent candles at front list
+          candleBuffer.insert(0, candle)
+
+          // limit candle buffer length to 24 hours
+          if (candleBuffer.length > 288) {
+            val removeNum = candleBuffer.length - 288
+            candleBuffer.remove(288, removeNum)
+          }
+        }
+
+      } else {
+        val candle = MarketCandle(time, periodMinutes, update.info.last)
+        candle.updateStatus(update.info)
+        candleBuffer.append(candle)
+      }
+
+      closeFn(MarketCandleClose(name, ClosePrice(time, update.info.last)))
+      //eventBus.publish(MarketEvent("/market/candle/close", MarketCandleClose(name, ClosePrice(time, update.info.last))))
+    }
+  }
+}
--- app/services/actors/CandleManagerActor.scala
+++ app/services/actors/CandleManagerActor.scala
@@ -1,20 +1,15 @@
 package services.actors
 
 // external
-import javax.inject.Inject
-
 import akka.actor.{Actor, ActorLogging}
-import models.poloniex.{MarketEvent, PoloniexEventBus}
-import models.poloniex.MarketEvent
+import javax.inject.Inject
 import play.api.Configuration
-import utils.Misc
-
-import scala.collection.mutable.ListBuffer
 import scala.language.postfixOps
 
 // internal
-import models.poloniex.{MarketCandle, MarketUpdate}
-import services.actors.ExponentialMovingAverageActor.MarketCandleClose
+import models.market.MarketCandles
+import models.poloniex.PoloniexEventBus
+import models.poloniex.{MarketEvent, MarketCandle, MarketUpdate}
 
 
 object CandleManagerActor {
@@ -30,12 +25,11 @@ object CandleManagerActor {
 class CandleManagerActor @Inject()(conf: Configuration) extends Actor with ActorLogging {
   import CandleManagerActor._
   import PoloniexCandleRetrieverActor._
-  import models.market.ClosePrice
 
   val eventBus = PoloniexEventBus()
-  val marketCandles = scala.collection.mutable.Map[String, ListBuffer[MarketCandle]]()
-  val candleTimeMinutes = 5
   val baseVolumeRule = conf.getInt("poloniex.candle.baseVolume").getOrElse(500)
+
+  val marketCandles2 = new MarketCandles()
 
   override def preStart() = {
     log info "subscribed to market updates"
@@ -54,37 +48,14 @@ class CandleManagerActor @Inject()(conf: Configuration) extends Actor with Actor
       updateMarket(update)
 
     case GetCandles(name) =>
-      marketCandles.get(name) match {
-        case Some(list) =>
-          sender ! list.toList.reverse
-        case None =>
-          sender ! List[MarketCandle]()
-      }
+      sender ! marketCandles2.getMarketCandles(name)
 
     case GetLastestCandle(name) =>
-      marketCandles.get(name) match {
-        case Some(list) =>
-          sender ! Some(list(0))
-        case None =>
-          sender ! None
-      }
+      sender ! marketCandles2.getLatestCandle(name)
 
     case SetCandles(name, last24hrCandles) =>
-      marketCandles.get(name) match {
-        case Some(candles) =>
-          log.info(s"Retrieved candle data for $name")
-          // This assumes that the candles received are sorted by time descending
-          val lastCandle = last24hrCandles.head
-          val currentCandle = candles.last
-          if (currentCandle.time.equals(lastCandle.time)) {
-            currentCandle.addCandle(lastCandle)
-            candles.appendAll(last24hrCandles.takeRight(last24hrCandles.length - 1))
-          } else {
-            candles.appendAll(last24hrCandles)
-          }
-        case None =>
-          log.error(s"received $name ${last24hrCandles.length} but nowhere to add them??")
-      }
+      marketCandles2.appendCandles(name, last24hrCandles)
+
   }
 
   private def updateMarket(update: MarketUpdate) = {
@@ -92,67 +63,17 @@ class CandleManagerActor @Inject()(conf: Configuration) extends Actor with Actor
     // only care about BTC markets
     if (name.startsWith("BTC")) {
 
-      // this is the current time rounded down to 5 minutes
-      val time = Misc.currentTimeRoundedDown(candleTimeMinutes)
-
-      // get candle collection for this market
-      val candleBuffer = marketCandles.get(name) match {
-        case Some(candles) => candles
-        case None =>
-          val candles = scala.collection.mutable.ListBuffer.empty[MarketCandle]
-          marketCandles.put(name, candles)
-
-          // send a message to the retriever to get the candle data from Poloniex
-          // if the 24 hour baseVolume from this update is greater than our threshold
-          if (update.info.baseVolume > baseVolumeRule) {
-              eventBus.publish(MarketEvent("/market/added", QueueMarket(name)))
-          }
-          candles
-      }
-
-      if (candleBuffer.length > 0) {
-        // examine candle at head
-        val currentCandle = candleBuffer.head
-
-        // are we still in the time period of the last candle that we created
-        if (currentCandle.time.isEqual(time)) {
-          // if so we need to update the info for the last candle
-          currentCandle.updateStatus(update.info)
-        } else {
-          // if we've skipped some candle periods due to no trade activity
-          // we need to fill in those missed periods
-          val skippedCandles = ((time.toEpochSecond - currentCandle.time.toEpochSecond) / (candleTimeMinutes * 60L)).toInt - 1
-          for (i <- 1 to skippedCandles) {
-            // add appropriate 5 minute interval to compute next candle time
-
-            val candleTime = currentCandle.time.plusMinutes(candleTimeMinutes * i)
-            val candle = MarketCandle(candleTime, candleTimeMinutes, currentCandle.close)
-
-            // inform the moving average actor of new close period
-            eventBus.publish(MarketEvent("/market/candle/close", MarketCandleClose(name, ClosePrice(candleTime, currentCandle.close))))
-            candleBuffer.insert(0, candle)
-          }
-
-          // start a new candle
-          val candle = MarketCandle(time, candleTimeMinutes, update.info.last)
-          candle.updateStatus(update.info)
-          // most recent candles at front list
-          candleBuffer.insert(0, candle)
-
-          // limit candle buffer length to 24 hours
-          if (candleBuffer.length > 288) {
-            val removeNum = candleBuffer.length - 288
-            candleBuffer.remove(288, removeNum)
-          }
+      if (!marketCandles2.containsMarket(name)) {
+        // send a message to the retriever to get the candle data from Poloniex
+        // if the 24 hour baseVolume from this update is greater than our threshold
+        if (update.info.baseVolume > baseVolumeRule) {
+          eventBus.publish(MarketEvent("/market/added", QueueMarket(name)))
         }
-
-      } else {
-        val candle = MarketCandle(time, candleTimeMinutes, update.info.last)
-        candle.updateStatus(update.info)
-        candleBuffer.append(candle)
       }
 
-      eventBus.publish(MarketEvent("/market/candle/close", MarketCandleClose(name, ClosePrice(time, update.info.last))))
+      marketCandles2.updateMarket(update){ candleClose =>
+        eventBus.publish(MarketEvent("/market/candle/close", candleClose))
+      }
     }
   }
 }
