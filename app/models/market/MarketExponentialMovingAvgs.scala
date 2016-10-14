0531da257767496cc271db39ba45cebefce8e896
Flowmaster <ellyssin.gimhae@gmail.com>
2016-10-13 22:49:51 -0600

    Added a new class for the exponential moving averages with tests.

---
+++ app/models/market/MarketExponentialMovingAvgs.scala
@@ -0,0 +1,148 @@
+package models.market
+
+import java.time.OffsetDateTime
+
+import utils.Misc
+
+import scala.math.BigDecimal.RoundingMode
+import scala.collection.mutable.ListBuffer
+
+object MarketExponentialMovingAvgs{
+
+  case class ExponentialMovingAverage(time: OffsetDateTime, ema: BigDecimal, atPrice: BigDecimal)
+}
+/**
+  * Keeps track of the exponential moving averages
+  * for each time period.
+  *
+  * @param marketName
+  * @param period how many periods for example 10 period, 5 period, etc
+  * @param periodMinutes length of period in minutes. Example 5
+  * @param historicClosePrices assumes the close prices are in decending order
+  *                            and that all close prices have the same periodMinutes
+  *                            as this class. Example, close prices for every 5 minute
+  *                            period for the last 24 hour window.
+  */
+class MarketExponentialMovingAvgs (val marketName: String,
+                                   val period: Int,
+                                   val periodMinutes: Int,
+                                   historicClosePrices: List[ClosePrice]) {
+
+  import MarketExponentialMovingAvgs._
+
+  require(period > 0)
+
+  // enough close prices needed to start the
+  // initial computation
+  require(historicClosePrices.length >= period)
+
+  val movingAverages = ListBuffer[ExponentialMovingAverage]()
+
+  setAverages(historicClosePrices)
+
+  /**
+    * Multiplier used for weighting exponential moving average.
+    *
+    * @param periodNum
+    * @return
+    */
+  private def multiplier(periodNum: Int) : BigDecimal = {
+    BigDecimal(2.0 / (periodNum + 1))
+  }
+
+  /**
+    * EMA: {Close - EMA(previous day)} x multiplier + EMA(previous day)
+    */
+  private def ema(close: BigDecimal, previousEMA: BigDecimal): BigDecimal = {
+    (close - previousEMA) * multiplier(period) + previousEMA
+  }
+
+  /**
+    * Sets the moving averages for market name based upon the received closing prices.
+    * The moving average time is dictated by the period field.
+    *
+    *  formula = {Close - EMA(previous day)} x multiplier + EMA(previous day)
+    *  source: http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:moving_averages
+    *
+    * @param closePrices - closing prices list MUST be ordered by descending date - i.e. most recent
+    *                    dated close price should be at head.
+    */
+  private def setAverages(closePrices: List[ClosePrice]) = {
+    // sum of the last close prices within the period
+    val sum = closePrices.takeRight(period).foldLeft(BigDecimal(0))( (a, p) => p.price + a)
+    val simpleMovingAverage = sum / period
+
+    val firstClose = closePrices(closePrices.length - period)
+
+    // exponential moving average begins with a simple moving average
+    ExponentialMovingAverage(firstClose.time, simpleMovingAverage, firstClose.price) +=: movingAverages
+
+    // loop through all closing prices from firstClose and compute exponential moving average
+    for (i <- period+1 to closePrices.length) {
+      val previousEMA = movingAverages.head.ema
+
+      val closePrice = closePrices(closePrices.length - i)
+
+      //val ema = (closePrice.price - previousEMA) * multiplier(period) + previousEMA
+      val a = ema(closePrice.price, previousEMA)
+      // most recent average always at head
+      ExponentialMovingAverage(closePrice.time, a.setScale(8, RoundingMode.CEILING), closePrice.price) +=: movingAverages
+
+    }
+  }
+
+  /**
+    * Updates the latest moving average. Assumes that the close price
+    * received is the latest close price.
+    *
+    * @param closePrice - the latest close price. ClosePrice timestamp must
+    *                   be after the timestamp of the latest average in moving
+    *                   averages.
+    * @return
+    */
+  def updateAverages(closePrice: ClosePrice): ExponentialMovingAverage = {
+    val normalizedTime = Misc.roundDateToMinute(closePrice.time, periodMinutes)
+    val latestAvg = movingAverages.head
+
+    val average =
+      if (latestAvg.time.isEqual(normalizedTime)) {
+        val previousEMA = movingAverages(1).ema
+        val a = ema(closePrice.price, previousEMA)
+        val expo = ExponentialMovingAverage(normalizedTime, a.setScale(8, RoundingMode.CEILING), closePrice.price)
+
+        // update the latest moving average at head
+        movingAverages.update(0, expo)
+        expo
+      } else {
+
+        // fill in skipped periods
+        if (latestAvg.time.plusMinutes(periodMinutes).isBefore(normalizedTime)) {
+          val deltaSeconds = normalizedTime.toEpochSecond - latestAvg.time.toEpochSecond
+          val skippedPeriods = (deltaSeconds / (periodMinutes * 60L)).toInt - 1
+          for (i <- 1 to skippedPeriods) {
+            // add appropriate 5 minute interval to compute next candle time
+            val previousEMA = movingAverages.head.ema
+            val lastClosePrice = movingAverages.head.atPrice
+            val time = latestAvg.time.plusMinutes(periodMinutes * i)
+            val a = ema(lastClosePrice, previousEMA)
+            ExponentialMovingAverage(time, a.setScale(8, RoundingMode.CEILING), lastClosePrice) +=: movingAverages
+          }
+        }
+
+         val previousEMA = movingAverages.head.ema
+         val a = ema(closePrice.price, previousEMA)
+         val expo = ExponentialMovingAverage(normalizedTime, a.setScale(8, RoundingMode.CEILING), closePrice.price)
+
+         // insert new average at head
+         expo +=: movingAverages
+         expo
+      }
+
+    // limit movingAverages to 24 hours
+    if (movingAverages.length > 288) {
+      val removeNum = movingAverages.length - 288
+      movingAverages.remove(288, removeNum)
+    }
+    average
+  }
+}
---
+++ test/models/ExponentialMovingAverageSpec.scala
@@ -0,0 +1,128 @@
+package models
+
+// external
+import org.scalatest._
+import org.scalatest.concurrent.ScalaFutures
+
+import scala.math.BigDecimal.RoundingMode
+
+// internal
+import models.market.{ClosePrice, ExponentialMovingAverages, MarketExponentialMovingAvgs}
+import utils.Misc
+
+
+class ExponentialMovingAverageSpec extends FlatSpec with ScalaFutures with BeforeAndAfter {
+
+  private def multiplier(periodNum: Int) : BigDecimal = {
+    BigDecimal(2.0 / (periodNum + 1))
+  }
+  private def ema(close: BigDecimal, previousEMA: BigDecimal, period: Int): BigDecimal = {
+    (close - previousEMA) * multiplier(period) + previousEMA
+  }
+
+
+  "MarketExponentialMovingAvgs" should "must be valid after init" in {
+    val minutes = 5
+    val period = 7
+    val time = Misc.currentTimeRoundedDown(minutes)
+    val prices = (for(i <- 0 until period) yield ClosePrice(time.minusMinutes(minutes * i), BigDecimal(i)))
+    val sum = prices.foldLeft(BigDecimal(0.0))( (a, v) => a + v.price )
+    val avg = sum / period
+
+    val sevenPeriodAverages = new MarketExponentialMovingAvgs("Test", period, minutes, prices.toList)
+    val average = sevenPeriodAverages.movingAverages.head
+
+    assert(average.time == prices.head.time)
+    assert(average.ema == avg)
+  }
+
+  "MarketExponentialMovingAvgs" should "init correctly when prices count is greater than period" in {
+
+    val minutes = 5
+    val period = 7
+    val time = Misc.currentTimeRoundedDown(minutes)
+    val prices = (for(i <- 0 to period) yield ClosePrice(time.minusMinutes(minutes * i), BigDecimal(i)))
+    val sum = prices.foldLeft(BigDecimal(0.0))( (a, v) => a + v.price )
+
+    val sevenPeriodAverages = new MarketExponentialMovingAvgs("Test", period, minutes, prices.toList)
+    val averages = sevenPeriodAverages.movingAverages
+    val avg = ema(prices.head.price, averages(1).ema, period)
+
+    assert (averages.length == 2)
+    assert (avg == averages.head.ema)
+    assert (prices.head.time == averages.head.time)
+  }
+
+  "MarketExponentialMovingAvgs" must "update correctly" in {
+
+    val minutes = 5
+    val period = 7
+    val time = Misc.currentTimeRoundedDown(minutes)
+    val prices = (for(i <- 0 to period) yield ClosePrice(time.minusMinutes(minutes * i), BigDecimal(i)))
+    val sum = prices.foldLeft(BigDecimal(0.0))( (a, v) => a + v.price )
+
+    val sevenPeriodAverages = new MarketExponentialMovingAvgs("Test", period, minutes, prices.toList)
+    for (i <- 1 to 5) {
+      val t = Misc.now().plusSeconds(i)
+      sevenPeriodAverages.updateAverages(ClosePrice(t, i))
+    }
+
+    val averages = sevenPeriodAverages.movingAverages
+    val avg = ema(prices.head.price, averages(1).ema, period)
+
+    assert (averages.length <= 3)
+    assert (avg < averages.head.ema)
+    assert (prices.head.time == averages.head.time)
+  }
+
+  "MarketExponentialMovingAvgs" should "add an average" in {
+
+    val minutes = 5
+    val period = 7
+    val time = Misc.currentTimeRoundedDown(minutes)
+    val prices = (for(i <- 0 to period) yield ClosePrice(time.minusMinutes(minutes * i), BigDecimal(i)))
+    val sum = prices.foldLeft(BigDecimal(0.0))( (a, v) => a + v.price )
+
+    val sevenPeriodAverages = new MarketExponentialMovingAvgs("Test", period, minutes, prices.toList)
+    val t = prices.head.time.plusMinutes(minutes+1)
+    val price = 1
+    sevenPeriodAverages.updateAverages(ClosePrice(t, price))
+
+    val averages = sevenPeriodAverages.movingAverages
+    val avg = ema(price, averages(0).ema, period)
+
+    assert (averages.length == 3)
+    assert (avg < averages.head.ema)
+    assert (averages.head.time == prices.head.time.plusMinutes(minutes))
+  }
+
+  "MarketExponentialMovingAvgs" should "add skipped periods" in {
+    val minutes = 5
+    val period = 5
+    val skipped = 20
+    val time = Misc.currentTimeRoundedDown(minutes)
+    val prices = (for(i <- 0 to period) yield ClosePrice(time.minusMinutes(minutes * i), BigDecimal(i)))
+    val lastPrice = prices.head.price
+    val sum = prices.foldLeft(BigDecimal(0.0))( (a, v) => a + v.price )
+
+    val sevenPeriodAverages = new MarketExponentialMovingAvgs("Test", period, minutes, prices.toList)
+    val averages = sevenPeriodAverages.movingAverages
+
+    val t = prices.head.time.plusMinutes(minutes * skipped + 3)
+    val normalizedTime = Misc.roundDateToMinute(t, minutes)
+    var lastEMA = averages.head.ema
+
+    for (i <- 0 until skipped - 1) {
+      lastEMA = ema(lastPrice, lastEMA, period).setScale(8, RoundingMode.CEILING)
+    }
+    val price = 1
+    lastEMA = ema(price, lastEMA, period).setScale(8, RoundingMode.CEILING)
+
+    val before = averages.length
+    sevenPeriodAverages.updateAverages(ClosePrice(t, price))
+
+    assert ( lastEMA == averages.head.ema)
+    assert (averages.length == skipped + before)
+    assert (averages.head.time == normalizedTime)
+  }
+}
