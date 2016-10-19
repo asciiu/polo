cca7da1aee74b7ca79e0a670517854be9adfc7fe
Flowmaster <ellyssin.gimhae@gmail.com>
2016-10-18 20:48:42 -0600

    Created a MarketCandle class with tests.

---
+++ app/models/market/MarketCandle.scala
@@ -0,0 +1,67 @@
+package models.market
+
+import java.time.OffsetDateTime
+
+import utils.Misc
+
+/**
+  * Created by bishop on 10/18/16.
+  */
+class MarketCandle (timestamp: OffsetDateTime,
+                    val timePeriodMinutes: Int) {
+
+  import Misc._
+
+  val time = roundDateToMinute(timestamp, timePeriodMinutes)
+
+  def this(closePrice: ClosePrice, periodMinutes: Int) {
+    this(closePrice.time, periodMinutes)
+    open = closePrice.price
+    close = closePrice.price
+    low = closePrice.price
+    high = closePrice.price
+  }
+
+  def this(time: OffsetDateTime, periodMinutes: Int, open: BigDecimal,
+           close: BigDecimal, high: BigDecimal, low: BigDecimal) {
+    this(time, periodMinutes)
+    this.open = open
+    this.close = close
+    this.high = high
+    this.low = low
+  }
+
+  var open: BigDecimal = 0
+  var low: BigDecimal = 0
+  var high: BigDecimal = 0
+  var close: BigDecimal = 0
+  //var volumeBtc: BigDecimal = 0
+
+  def isBuy(): Boolean = close > open
+
+  def +=(candle: MarketCandle): MarketCandle = {
+    if (isTimePeriod(candle.time)) {
+      if (candle.low < low || low == 0) low = candle.low
+      if (candle.high > high || high == 0) high = candle.high
+    }
+    this
+  }
+
+  /**
+    * Assumes the close price time is normalized
+    *
+    * @param closePrice
+    */
+  def update(closePrice: ClosePrice) = {
+    if (isTimePeriod(closePrice.time)) {
+      if (closePrice.price < low || low == 0) low = closePrice.price
+      if (closePrice.price > high || high == 0) high = closePrice.price
+      close = closePrice.price
+    }
+  }
+
+  def isTimePeriod(time: OffsetDateTime) : Boolean = {
+    val normalizedTime = roundDateToMinute(time, timePeriodMinutes)
+    this.time.isEqual(normalizedTime)
+  }
+}
---
+++ test/models/MarketCandleSpec.scala
@@ -0,0 +1,134 @@
+package models
+
+import java.time.OffsetDateTime
+import models.market.{ClosePrice, MarketCandle}
+import org.scalatest.FlatSpec
+import utils.Misc
+
+
+class MarketCandleSpec extends FlatSpec {
+  import Misc._
+
+  it should "create MarketCandle object from close price" in {
+    val periodMinute = 5
+    val price = BigDecimal(0.01)
+    val time = now()
+    val closePrice = ClosePrice(now(), price)
+    val normalizedTime = roundDateToMinute(time, periodMinute)
+
+    val candle = new MarketCandle(closePrice, 5)
+
+    assert (candle.timePeriodMinutes === periodMinute)
+    assert (candle.open === price)
+    assert (candle.close === price)
+    assert (candle.high === price)
+    assert (candle.low === price)
+    assert (candle.time.isEqual(normalizedTime))
+  }
+
+  it should "create MarketCandle object from low, high, etc" in {
+    val periodMinute = 5
+    val price = BigDecimal(0.01)
+    val open = price
+    val close = price + 0.000044
+    val high = price + 0.00033
+    val low = price - 0.0001
+
+    val time = now()
+    val normalizedTime = roundDateToMinute(time, periodMinute)
+
+    val candle = new MarketCandle(time, periodMinute, open, close, high, low)
+
+    assert (candle.timePeriodMinutes === periodMinute)
+    assert (candle.open === open)
+    assert (candle.close === close)
+    assert (candle.high === high)
+    assert (candle.low === low)
+    assert (candle.time.isEqual(normalizedTime))
+
+  }
+
+  it should "update using the += operator with same time periods" in {
+    val periodMinute = 5
+    val price = BigDecimal(0.01)
+    val open = price
+    val close = price + 0.000044
+    val high = price + 0.00033
+    val higher = high + 1
+    val low = price - 0.0001
+    val lower = low - 0.00001
+
+    val time1 = OffsetDateTime.parse("2016-10-19T02:26:15.110Z")
+    val time2 = time1.plusSeconds(5)
+    val normalizedTime = roundDateToMinute(time1, periodMinute)
+
+    val candle1 = new MarketCandle(time1, periodMinute, open, close, high, low)
+    val candle2 = new MarketCandle(time2, periodMinute, open, close, higher, lower)
+
+    candle1 += candle2
+    assert (candle1.open == open)
+    assert (candle1.close == close)
+    assert (candle1.high == candle2.high)
+    assert (candle1.low == candle2.low)
+    assert (candle2.high == higher)
+    assert (candle2.low == lower)
+  }
+
+  it should "not update using the += operator when the time periods are different" in {
+    val periodMinute = 5
+    val price = BigDecimal(0.01)
+    val open = price
+    val close = price + 0.000044
+    val high = price + 0.00033
+    val higher = high + 1
+    val low = price - 0.0001
+    val lower = low - 0.00001
+    val time1 = OffsetDateTime.parse("2016-10-19T02:26:55.110Z")
+    val time2 = time1.plusMinutes(periodMinute)
+    val normalizedTime = roundDateToMinute(time1, periodMinute)
+
+    val candle1 = new MarketCandle(time1, periodMinute, open, close, high, low)
+    val candle2 = new MarketCandle(time2, periodMinute, open, close, higher, lower)
+
+    candle1 += candle2
+    assert (candle1.open == open)
+    assert (candle1.close == close)
+    assert (candle1.high == high)
+    assert (candle1.low == low)
+    assert (candle2.high == higher)
+    assert (candle2.low == lower)
+  }
+
+  it should "return true if same time period" in {
+    val time = now()
+
+    val candle = new MarketCandle(time, 5)
+
+    assert(candle.isTimePeriod(time))
+  }
+
+  it should "update when the close price time is the same" in {
+    val periodMinute = 5
+    val price = BigDecimal(0.01)
+    val open = price
+    val close = price + 0.000044
+    val high = price + 0.00033
+    val low = price - 0.0001
+    val time1 = OffsetDateTime.parse("2016-10-19T02:26:55.110Z")
+
+    val candle = new MarketCandle(time1, periodMinute, open, close, high, low)
+    val newLow = low - 0.000005748
+    candle.update(ClosePrice(time1.plusSeconds(45), newLow))
+
+    assert (candle.low == newLow)
+    assert (candle.high == high)
+    assert (candle.close == newLow)
+
+    val newHigh = high + 0.000005748
+    candle.update(ClosePrice(time1.plusSeconds(120), newHigh))
+
+    assert (candle.low == newLow)
+    assert (candle.high == newHigh)
+    assert (candle.close == newHigh)
+  }
+}
